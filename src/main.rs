9bae75d4cbf44c6e32d09064fcf0a5702241d345
Epsylene <epsylene@gmail.com>
2023-06-04 02:42:18 +0200

    CPU basic run loop + first opcodes

--- src/main.rs
+++ src/main.rs
@@ -67,10 +67,130 @@ pub struct CPU {
     pub program_counter: u16,
     pub accumulator: u8,
     pub status: u8,
+    pub register_x: u8,
 }
 
 impl CPU {
+    pub fn new() -> Self {
+        Self {
+            program_counter: 0,
+            accumulator: 0,
+            status: 0,
+            register_x: 0,
+        }
+    }
+
+    pub fn interpret(&mut self, program: Vec<u8>) {
+        self.program_counter = 0;
+
+        loop {
+            let opcode = program[self.program_counter as usize];
+            self.program_counter += 1;
+
+            match opcode {
+                // BRK (Break): forces an interrupt
+                // request
+                0x00 => return,
+
+                // LDA (Load Accumulator): loads a byte of
+                // memory into the accumulator.
+                0xA9 => {
+                    // The command parameter is the next byte
+                    // after the opcode itself
+                    let param = program[self.program_counter as usize];
+                    self.program_counter += 1;
+                    
+                    self.accumulator = param;
+                    self.zero_negative(self.accumulator)
+                }
+
+                // TAX (Transfer Accumulator to X): copies the
+                // current contents of the accumulator into the
+                // X register.
+                0xAA => {
+                    self.register_x = self.accumulator;
+                    self.zero_negative(self.register_x);
+                }
+
+                // INX (Increment X Register): add one to the X
+                // register.
+                0xE8 => {
+                    self.register_x = self.register_x.wrapping_add(1);
+                    self.zero_negative(self.register_x);
+                }
+
+                _ => todo!(),
+            }
+        }
+    }
+
+    /// Update the zero and negative flags of the status
+    /// register depending on the contents of the given register
+    fn zero_negative(&mut self, register: u8) {
+        // If the register is 0, set the zero flag, otherwise
+        // clear it
+        if register == 0 {
+            self.status |= 0b0000_0010;
+        } else {
+            self.status &= 0b1111_1101;
+        }
+
+        // Set the negative flag if the negative bit of the
+        // register is set
+        if register & 0b1000_0000 != 0 {
+            self.status |= 0b1000_0000;
+        } else {
+            self.status &= 0b0111_1111;
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_0xa9_lda_immediate_load_data() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0x05, 0x00]);
+
+        assert_eq!(cpu.accumulator, 0x05);
+        assert!(cpu.status & 0b0000_0010 == 0b00);
+        assert!(cpu.status & 0b1000_0000 == 0);
+    }
+
+    #[test]
+    fn test_0xa9_lda_zero_flag() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0x00, 0x00]);
+        assert!(cpu.status & 0b0000_0010 == 0b10);
+    }
+
+    #[test]
+    fn test_0xaa_tax_move_a_to_x() {
+        let mut cpu = CPU::new();
+        cpu.accumulator = 10;
+        cpu.interpret(vec![0xaa, 0x00]);
+
+        assert_eq!(cpu.register_x, 10)
+    }
     
+    #[test]
+    fn test_5_ops_working_together() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+
+        assert_eq!(cpu.register_x, 0xc1)
+    }
+
+    #[test]
+    fn test_inx_overflow() {
+        let mut cpu = CPU::new();
+        cpu.register_x = 0xff;
+        cpu.interpret(vec![0xe8, 0x00]);
+
+        assert_eq!(cpu.register_x, 0)
+    }
 }
 
 fn main() {
